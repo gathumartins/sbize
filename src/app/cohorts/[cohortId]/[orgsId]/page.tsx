781efc5e2a8798d3d22ca28ac404c5d68efb9971
gathumartins <gathumartins@gmail.com>
2024-09-26 10:06:17 +0300

    fixed container type in organization page

--- src/app/cohorts/[cohortId]/[orgsId]/page.tsx
+++ src/app/cohorts/[cohortId]/[orgsId]/page.tsx
@@ -36,7 +36,7 @@ async function page({params}:any) {
     <main>
         <Hero heroTitle={data.data.post.title} bg={bg}/>
         <article className="py-10">
-          <section className="conatiner">
+          <section className="container">
             <div className="max-w-[1000px] font-mainFont mx-auto [&_h2]:text-3xl [&_h2]:mb-4 [&_h2]:mt-5 [&_h2]:text-sprimary [&_h2]:capitalize [&_h2]:font-bold [&_p]:mb-3 [&_p]:text-lg [&_iframe]:my-7 [&_iframe]:rounded-lg [&_iframe]:aspect-video [&_iframe]:w-full [&_a]:orangeBtn [&_a]:my-5 [&_a]:inline-block [&_a]:w-[180px]" dangerouslySetInnerHTML={{__html:data.data.post.content}}></div>
           </section>
         </article>
